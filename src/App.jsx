495cbe4a6f6ff88dbdf4c2c45a9dbe73d4b6490f
Gabriel de Paula <gabriel.meira.2004@gmail.com>
2022-10-07 12:26:47 -0300

    ♻️ refactored weatherData request

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import usePersistedState from './utils/customHooks/usePersistedState';
 import getWeatherData from './utils/requestWeather';
-import { getTimeNow, getISOIntervalFromNow } from './utils/ISO8601';
+import { getTimeNow } from './utils/ISO8601';
 
 import WeatherBackground from './components/WeatherBackground';
 
 function App() {
 	const TIME_INTERVAL_HOURS = 6;
 
-	const [APIdata, setAPIdata] = usePersistedState('previousAPIdata');
+	const [weatherData, setWeatherData] = usePersistedState('previousAPIdata');
 	useEffect(() => {
-		const previousDataIsValid = !APIdata
-			? false
-			: !(getTimeNow().raw.getTime() > new Date(APIdata[6].startTime).getTime() + 1000 * 60 * 60); // checks if data is more than 1 hour late
+		const previousDataIsValid =
+			weatherData &&
+			getTimeNow().raw.getTime() <
+				new Date(weatherData[TIME_INTERVAL_HOURS].time).getTime() + 1000 * 60 * 60; // checks if data is more than 1 hour late
 		if (!previousDataIsValid) {
-			const options = {
-				apikey: 'K0BKOLcvnW2dswAega7fkYkTm4eTKv1T',
-				fields: [
-					'windSpeed',
-					'temperature',
-					'humidity',
-					'cloudCover',
-					'sunriseTime',
-					'sunsetTime',
-					'rainIntensity',
-					'freezingRainIntensity',
-					'snowIntensity',
-					'precipitationType',
-					'weatherCode', // get thunderstorms
-				],
-				location: [-21.129384, -44.246503],
-				interval: getISOIntervalFromNow(TIME_INTERVAL_HOURS),
-				timesteps: '1h',
-				units: 'metric',
-			};
-			getWeatherData(options).then((data) => setAPIdata(data));
+			getWeatherData(TIME_INTERVAL_HOURS).then((data) => setWeatherData(data));
 		}
 	});
-
-	const [weatherData, setWeatherData] = useState();
-	useEffect(() => {
-		if (APIdata) {
-			const tempData = [];
-			APIdata.forEach((interval) => {
-				const tempObject = { ...interval.values };
-				Object.assign(tempObject, { time: interval.startTime });
-				tempData.push(tempObject);
-			});
-			setWeatherData(tempData);
-		}
-	}, [APIdata]);
 
 	return <div>{weatherData && <WeatherBackground weatherData={weatherData[6]} />}</div>;
 }
--- src/utils/requestWeather.js
+++ src/utils/requestWeather.js
@@ -1,16 +1,47 @@
 import axios from 'axios';
+import { getISOIntervalFromNow } from './ISO8601';
 
-async function getWeatherData(options) {
-	const promise = axios.get(
-		`https://api.tomorrow.io/v4/timelines/?location=${options.location.join()}&fields=${options.fields.join()}&timesteps=${
-			options.timesteps
-		}&units=${options.units}&startTime=${options.interval[0]}&endTime=${
-			options.interval[1]
-		}&apikey=${options.apikey}
-            `
-	);
+async function getWeatherData(timeInterval) {
+	const options = {
+		apikey: 'K0BKOLcvnW2dswAega7fkYkTm4eTKv1T',
+		// apikey: '88m0aqRGnU9HxVeMenXLYhhsB7UIUA4a',
+		fields: [
+			'windSpeed',
+			'temperature',
+			'humidity',
+			'cloudCover',
+			'sunriseTime',
+			'sunsetTime',
+			'rainIntensity',
+			'freezingRainIntensity',
+			'snowIntensity',
+			'precipitationType',
+			'weatherCode', // get thunderstorms
+		],
+		location: [-21.129384, -44.246503],
+		interval: getISOIntervalFromNow(timeInterval),
+		timesteps: '1h',
+		units: 'metric',
+	};
 
-	const dataPromise = promise.then((response) => response.data.data.timelines[0].intervals);
+	const url = `https://api.tomorrow.io/v4/timelines/?location=${options.location.join()}&fields=${options.fields.join()}&timesteps=${
+		options.timesteps
+	}&units=${options.units}&startTime=${options.interval[0]}&endTime=${options.interval[1]}&apikey=${
+		options.apikey
+	}
+	`;
+
+	const promise = axios.get(url);
+	const dataPromise = promise.then((response) => {
+		const originalData = response.data.data.timelines[0].intervals;
+		const tempData = [];
+		originalData.forEach((interval) => {
+			const tempObject = { ...interval.values };
+			Object.assign(tempObject, { time: interval.startTime });
+			tempData.push(tempObject);
+		});
+		return tempData;
+	});
 
 	return dataPromise;
 }
